e6d316369e671d0b99946ae18a59cf97afe9689b
ThalissonTSR <thalissontharl@gmail.com>
2025-05-08 10:21:22 -0300

    update

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -7,8 +7,9 @@ import { CardPanelContentStyled, ItemStyled } from "./style";
 
 const navigatorHasShare = navigator.share;
 
-function Panel({ updateAt, onChange, data, country, getCoviddata }) {
-    const { cases, recovered, deaths, todayCases, todayDeaths } = data;
+function Panel({ updateAt, onChange, data, country }) {
+    const { recovered } = data;
+
 
     const renderCountries = (country, index) => (
         <MenuItem key={`country-${index}`} value={country.value}>
